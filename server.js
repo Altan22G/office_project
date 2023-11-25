07bfddf30e1b642010d349cdc8af9654f471e96a
Altan22G <petraltan2008@gmail.com>
2023-11-25 23:57:17 +0200

    gamw ton server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'admin_p.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Ο Server τρέχει στο http://localhost:${port}`);
+});
