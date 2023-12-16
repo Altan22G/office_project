e5e07f36f20996a920b61a8e58e970d7b2cfdd33
Altan22G <petraltan2008@gmail.com>
2023-12-16 14:12:43 +0200

    collaborator

--- public/main.js
+++ public/main.js
@@ -1,15 +1,28 @@
-"use strict";
-const observer = new IntersectionObserver((entries) =>{
-  entries.forEach((entry) =>{
-    console.log(entry)
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show')
-    } else {
-      entry.target.classList.remove('show')
-    }
-  });
-});
+function sendMail(event) {
+  event.preventDefault();
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
 
+  fetch('/submitContactForm', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+      message: message,
+    }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+      alert('Το μήνυμά σας στάλθηκε με επιτυχία!');
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      alert('Υπήρξε ένα πρόβλημα κατά την αποστολή του μηνύματος.');
+    });
+  }
--- server.js
+++ server.js
@@ -7,15 +7,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Ορίζουμε τον transporter για την αποστολή email
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'your_email@gmail.com', // Το email από το οποίο θα στέλνετε τα email
-    pass: 'your_email_password' // Ο κωδικός του email από το οποίο θα στέλνετε τα email
-  }
-});
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,13 +15,6 @@ let formData;  // Μετακινήστε τη δήλωση έξω από το ap
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.post('/submitForm', (req, res) => {
-  formData = req.body;  // Εδώ ορίζεται το formData
-  console.log(formData);
-
-  res.sendFile(path.join(__dirname, 'src', 'views', 'car.html'));
 });
 
 app.get('/car', (req, res) => {
@@ -40,15 +25,21 @@ app.get('/contact', (req, res) => {
   res.sendFile(path.join(__dirname, 'src', 'views', 'contact.html'));
 });
 
-app.post('/submitInsuranceForm', (req, res) => {
-  // Εδώ μπορείτε να χρησιμοποιήσετε το formData
-  console.log(formData);
-  res.send('Τα δεδομένα σας έχουν ληφθεί με επιτυχία!');
+// Ορίζουμε τον transporter για την αποστολή email
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'petraltan2008@gmail.com', // Το email από το οποίο θα στέλνετε τα email
+    pass: '@ltpetr2008' // Ο κωδικός του email από το οποίο θα στέλνετε τα email
+  }
 });
 
-app.post('/submitContactForm', (req, res) => {
-  // Εδώ μπορείτε να χρησιμοποιήσετε το formData
+app.post('/submitContactForm', handleContactForm);
+
+function handleContactForm(req, res) {
+  formData = req.body;  // Εδώ ορίζεται το formData
   console.log(formData);
+
   // Στέλνουμε το email
   const email = formData.email;
   const mailOptions = {
@@ -61,13 +52,13 @@ app.post('/submitContactForm', (req, res) => {
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error(error);
-      res.send('Υπήρξε ένα πρόβλημα κατά την αποστολή του μηνύματος.');
+      res.status(500).send('Υπήρξε ένα πρόβλημα κατά την αποστολή του μηνύματος.');
     } else {
       console.log('Email sent: ' + info.response);
-      res.send('Το μήνυμά σας στάλθηκε με επιτυχία.');
+      res.status(200).send('Το μήνυμά σας στάλθηκε με επιτυχία.');
     }
   });
-});
+}
 
 app.listen(port, () => {
   console.log(`Ο Server τρέχει στο http://localhost:${port}`);
